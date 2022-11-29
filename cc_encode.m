95397f443ae87070f7e18aae030a12d6d6f6c487
Victorine Penot <vpenot001@bordeaux-inp.fr>
2022-11-29 16:27:38 +0100

    cc-encode OKAAAAAAAAAYYYYYYYYYYYYYYYYYYYYYYYY

--- cc_encode.m
+++ cc_encode.m
@@ -3,7 +3,7 @@ function [c,s_f] = cc_encode(u,treillis,s_i,closed);
 cpt=1;
 c=zeros(1,2*length(u));
 while(closed==false)
-    tmp=s_i+1;
+    tmp=s_i+1
     for i=1:2:2*length(u)
         dec=de2bi(treillis.outputs(tmp,u(cpt)+1),'left-msb')
         if length(dec)==1
@@ -13,7 +13,10 @@ while(closed==false)
             c(i)=dec(1);
             c(i+1)=dec(2);
         end
-        tmp=treillis.outputs(tmp,u(cpt)+1)
+        
+        cpt=cpt
+        u_ind=u(cpt)+1
+        tmp=treillis.nextStates(tmp,u(cpt)+1)+1
         cpt=cpt+1;
         if (cpt==length(u))
             closed=true;
--- main.m
+++ main.m
@@ -5,6 +5,8 @@ clc,
 treillis=poly2trellis(3,[5,7]);
 u=[1,1,0,1,0];
 s_i=0;
+test=convenc(u,treillis,s_i);
 closed=false;
 res=cc_encode(u,treillis,s_i,closed);
-
+test=convenc(u,treillis,s_i);
+resultat=res==test;
